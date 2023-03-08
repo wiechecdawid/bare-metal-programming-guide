5124006ff34dae550d9fcb3c41cb075e428eeda3
wiechecdawid <wiechec.dawid@gmail.com>
2023-03-08 23:37:26 +0100

    blink led

--- own-code/main.c
+++ own-code/main.c
@@ -1,5 +1,58 @@
+#include <inttypes.h>
+#include <stdbool.h>
+
+#define BIT(x) (1UL << (x))
+#define PIN(bank, num) ((((bank) - 'A') << 8) | (num))
+#define PINNUM(pin) (pin & 255)
+#define PINBANK(pin) (pin >> 8)
+
+struct gpio {
+  volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
+};
+
+#define GPIO(bank)((struct gpio *) (0x40020000 + 0x400 * (bank)))
+
+enum { GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG };
+
+static inline void gpio_set_mode(uint16_t pin, uint8_t mode) {
+    struct gpio *gpio = GPIO(PINBANK(pin));
+    int pinNum = PINNUM(pin);
+    gpio->MODER &= ~(3U << (pinNum * 2));
+    gpio->MODER |= (mode & 3) << (pinNum * 2);
+}
+
+static inline void gpio_write(uint16_t pin, bool val){
+    struct gpio *gpio = GPIO(PINBANK(pin));
+    gpio->BSRR = (1u << PINNUM(pin)) << (val ? 0 : 16);
+}
+
+static inline void spin(volatile uint32_t count) {
+  while (count--) (void) 0;
+}
+
+// STM 32 in order to save power has GPIOs disabled by default. GPIO needs to be clocked with RCC in order to be enabled
+// define RCC unit
+struct rcc {
+    volatile uint32_t CR, PLLCFGR, CFGR, CIR, AHB1RSTR, AHB2RSTR, AHB3RSTR, RESERVED0, APB1RSTR, APB2RSTR, RESERVED1[2], AHB1ENR, AHB2ENR, AHB3ENR,
+        RESERVED2, APB1ENR, APB2ENR, RESERVED3[2], AHB1LPENR, AHB2LPENR, AHB3LPENR, RESERVED4, APB1LPENR, APB2LPENR, RESERVED5[2], BDCR, CSR,
+        RESERVED6[2], SSCGR, PLLI2SCFGR;
+};
+
+#define RCC ((struct rcc *) 0x40023800)
+
 int main(void) {
-    return 0; // Do nothing yet
+    uint16_t ledPin = PIN('A', 5);
+    RCC->AHB1ENR |= BIT(PINBANK(ledPin));
+    gpio_set_mode(ledPin, GPIO_MODE_OUTPUT);
+    
+    for (;;) {
+        gpio_write(ledPin, true);
+        spin(0x003FFFFF);
+        gpio_write(ledPin, false);
+        spin(0x003FFFFF);
+    }
+
+    return 0;
 }
 
 // Startup code
