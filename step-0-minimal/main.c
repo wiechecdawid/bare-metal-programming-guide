6a7446516983fd0490e590cbe1e7ef37c949708c
wiechecdawid <wiechec.dawid@gmail.com>
2023-03-08 01:45:06 +0100

    MCU boot and vector table

--- step-0-minimal/main.c
+++ step-0-minimal/main.c
@@ -1,7 +1,34 @@
 // Copyright (c) 2022 Cesanta Software Limited
 // All rights reserved
 
+#include <inttypes.h>
+
+// #define GPIOA_BASE_ADDRESS 0x40020000U
+// #define MODER_OFFSET 0X0U
+// #define GPIOA_MODER_ADDR (GPIOA_BASE_ADDRESS + MODER_OFFSET)
+#define PIN(bank, num) ((((bank) - 'A') << 8) | (num))
+#define PINNO(pin) (pin & 255)
+#define PINBANK(pin) (pin >> 8)
+
+struct gpio {
+  volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2]
+};
+
+// #define GPIOA ((struct gpio *) 0x40020000)
+#define GPIO(bank) ((struct gpio *)(0x40020000 + 0x400 * (bank)))
+
+enum { GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG };
+
+static inline void gpio_set_mode(uint8_t pin, uint8_t mode) {
+  struct gpio *gpio = GPIO(PINBANK(pin));
+  uint8_t pinNo = PINNO(pin);
+  gpio->MODER &= ~(3U << (pinNo * 2));
+  gpio->MODER |= (mode & 3) << (pinNo * 2);
+}
+
 int main(void) {
+  uint16_t pin = PIN('A', 3);
+  gpio_set_mode(pin, GPIO_MODE_OUTPUT);
   return 0;
 }
 
